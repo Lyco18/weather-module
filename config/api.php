9ff6fb48f9d62019d18a709c3b777f66bc460015
Lyco18 <lydiacocozza@hotmail.com>
2019-12-16 20:15:05 +1000

    api-key file

---
+++ config/api.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ *
+ *
+ */
+
+return [
+    //IpStack -> http://api.ipstack.com/{$ip}?access_key={$apiKey}
+    "ipstack" => [
+        "key" => "xxxxx",
+    ],
+    //DarkSky -> https://api.darksky.net/forecast/{$apiKey}/{$lat},{$long},{$dates[$i]}
+    "darksky" => [
+        "key" => "xxxxx",
+    ],
+    //Opencage ->  https://api.opencagedata.com/geocode/v1/json?q=$search&key=$apiKeyOpenCage
+    "opencage" => [
+        "key" => "xxxxx",
+    ]
+];
